05f9e569c37220571eb5877bdc4aa4cfc59365fa
João Pedro <jpresende502@gmail.com>
2020-10-10 23:49:24 -0300

    eu nem testei, se funcionar meua migo.

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function message(target, context, message, isBot) {
     }
 
     if (String(msgBan).startsWith("!ban")) {
+        let randomBan = Math.floor(Math.random() * 10000)
         let msgRandom = Math.floor(Math.random() * 5) + 1;
         let alvoBan = String(mensagem).split(" ");
         if (alvoBan[1] == undefined) {
@@ -72,33 +73,42 @@ function message(target, context, message, isBot) {
             case 1:
               client.say(
                 target,
-                `/me ${alvoBan[1]} foi banido por falar que php é ruim Kappa`
+                `/me ${context.username} baniu ${alvoBan[1]} por falar que php é ruim Kappa`
               );
               break;
             case 2:
               client.say(
                 target,
-                `/me ${alvoBan[1]} foi banido por não ter feito bootcamp de 2dias de js >( `
+                `/me ${context.username} baniu ${alvoBan[1]} por não ter feito bootcamp de 2dias de js >( `
               );
               break;
             case 3:
-              client.say(target, `/me ${alvoBan[1]} foi banido por usar windows LUL`);
+              client.say(target, `/me ${context.username} baniu ${alvoBan[1]} por usar windows LUL`);
               break;
             case 4:
-              client.say(target, `/me ${alvoBan[1]} foi banido por usar js Kappa`);
+              client.say(target, `/me ${context.username} baniu ${alvoBan[1]} por usar js Kappa`);
               break;
             case 5:
               client.say(
                 target,
-                `/me ${alvoBan[1]} foi banido por não deixar no lurk...`
+                `/me ${context.username} baniu ${alvoBan[1]} por não deixar no lurk...`
               );
               break;
             default:
               client.say(
                 target,
-                `/me ${alvoBan[1]} foi banido por falar que java é bom Kappa`
+                `/me ${context.username} baniu ${alvoBan[1]} por falar que java é bom Kappa`
               );
           }
+        }
+        if(randomBan > 100 && randomBan < 1000){
+          client.say(target, `/timeout ${alvoBan[1]} 10`)
+          client.say(target, `${alvoBan[1]} foi banido`)
+        } else if (randomBan > 1000 && randomBan < 7000){
+            client.say(target, `/timeout ${context.username} 10`)
+            client.say(target, `${context.username} foi banido`)
+        } else {
+            client.say(target, `Ninguem foi banido.`)
         }
   }
 }
