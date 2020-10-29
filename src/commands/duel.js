1e264a0d906058ce3b86fdb2fea38618ffa0634f
kastrowalker <victorcsa2002@gmail.com>
2020-10-29 12:02:58 -0300

    Resolvendo bug do duelo com deninho

--- src/commands/duel.js
+++ src/commands/duel.js
@@ -1,15 +1,9 @@
 const COMMAND = '!duel @deninhobot';
 
-function sayFunc(client, target) {
-  client.say(target, `!accept`);
-}
-
-function startFunction(client, target) {
-  return setTimeout(sayFunc(client, target), 2000);
-}
-
 exports.default = (client, target, context, message) => {
   if (message === COMMAND) {
-    startFunction(client, target);
+    setTimeout(() => {
+      client.say(target, '!accept');
+    }, 2000);
   }
 };
