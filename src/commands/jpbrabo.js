71d72eeebfc4827e95458ffc024d313982097fde
Renan Gomes <renan.gomes@live.com>
2020-10-30 17:05:53 -0300

    Movendo !jp/!brabo para o arquivo de divulgações.

--- src/commands/jpbrabo.js
+++
@@ -1,7 +0,0 @@
-exports.default = (client, target, _, message) => {
-  const trimMessage = message.trim();
-
-  if (trimMessage === '!jp' || trimMessage === '!brabo') {
-    client.say(target, `/me É isso! SeemsGood https://twitch.tv/jpbrab0`);
-  }
-};
