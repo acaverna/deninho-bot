508eff8d6c695b9454c2ff9654dc676ada5550f5
VitthorDias <v.fernandes@academico.ifg.edu.br>
2020-11-10 21:01:06 -0300

    Corrigindo bug no !duel

--- src/commands/duel.js
+++ src/commands/duel.js
@@ -4,8 +4,7 @@ exports.default = (client, target, context, message) => {
   const splittedMessage = String(message).split(' ');
 
   if (
-    (splittedMessage[0] === COMMAND && splittedMessage[1] === 'deninhobot') ||
-    splittedMessage[1] === '@deninhobot'
+    (splittedMessage[0] === COMMAND && splittedMessage[1] === 'deninhobot') || splittedMessage[0] === COMMAND && splittedMessage[1] === '@deninhobot'
   ) {
     setTimeout(() => {
       client.say(target, '!accept');
