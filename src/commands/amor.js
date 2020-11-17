d7aedd73858dcff114e597427ee33f7ee1585d83
kastrowalker <victorcsa2002@gmail.com>
2020-11-17 11:14:13 -0300

    Criando comando amor

---
+++ src/commands/amor.js
@@ -0,0 +1,10 @@
+const COMMAND = '!amor';
+
+exports.default = (client, target, context, message) => {
+  if (message === COMMAND) {
+    client.say(
+      target,
+      'pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv',
+    );
+  }
+};
