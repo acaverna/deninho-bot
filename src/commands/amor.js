b75e0360f242a14f13b3af8934d7efe397780d3e
pachi <pachicodes@gmail.com>
2020-12-17 09:46:42 -0500

    love

--- src/commands/amor.js
+++ src/commands/amor.js
@@ -1,7 +1,7 @@
 const COMMAND = '!amor';
 
 exports.default = (client, target, context, message) => {
-  if (message === COMMAND) {
+  if (message === COMMAND || message === '!love') {
     client.say(
       target,
       'pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv pachic2Luv',
